e7464dd0055c0c6517977f58105a29a26ef4dbe9
markelcenda <Markel@markelcenda>
2020-03-30 11:50:17 +0200

    GetAll metodoa implementatuta

--- src/modelo/dao/ModeloProducto.java
+++ src/modelo/dao/ModeloProducto.java
@@ -1,15 +1,51 @@
 package modelo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Producto;
 import modelo.bean.Talla;
 
-public class ModeloProducto {
+public class ModeloProducto extends Conector{
 
 	public ArrayList<Producto> getAll(){
 		//TODO implementar
-		return null;
+		
+		ArrayList<Producto> productos =new ArrayList<Producto>();
+		
+		try {
+			
+			Statement st=super.conexion.createStatement();
+			ResultSet rs=st.executeQuery("select * from productos");
+			
+			while(rs.next()) {
+				
+				Producto producto=new Producto();
+				
+				producto.setId(rs.getInt("id"));
+				producto.setNombre(rs.getString("nombre"));
+				producto.setStock(rs.getInt("stock"));
+				producto.setFecha_compra(rs.getDate("fecha_compra"));
+				producto.setColor(rs.getString("color"));
+				producto.setMade_in(rs.getString("made_in"));
+				producto.setPrecio(rs.getDouble("precio"));
+				producto.setDescuento(rs.getInt("descuento"));
+				producto.setTallas(rs.getString("tallas"));
+				
+				productos.add(producto);
+				
+			}
+			
+			return productos;
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return productos;
 	}
 	
 	public Producto get(int id) {
