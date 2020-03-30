e468ccf04c80c04131977dec027cfd4588897648
markelcenda <Markel@markelcenda>
2020-03-30 11:50:47 +0200

    VerProductos servleta eginda

--- src/controladores/VerProductos.java
+++ src/controladores/VerProductos.java
@@ -1,11 +1,16 @@
 package controladores;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import modelo.bean.Producto;
+import modelo.dao.ModeloProducto;
 
 /**
  * Servlet implementation class VerProductos
@@ -26,6 +31,11 @@ public class VerProductos extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		ModeloProducto modeloProducto=new ModeloProducto();
+		ArrayList<Producto> productos=modeloProducto.getAll();
+		
+		request.setAttribute("productos", productos);
 		
 		request.getRequestDispatcher("verProductos.jsp").forward(request, response);
 		
